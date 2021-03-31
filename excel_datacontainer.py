0653763f84f57df9bf06b801bf3a188545b57966
OLGJ <olof.josefsson@outlook.com>
2021-03-31 14:21:44 +0200

    Added excel file creater and document.

---
+++ excel_datacontainer.py
@@ -0,0 +1,21 @@
+### This file creates an excel document for the collected data.
+
+#Imports
+from openpyxl import Workbook
+
+#Create workbook
+workbook_name = 'wsb_data.xlsx'
+
+titles = ["DD", "Discussion", "YOLO", "News", "Earningsthread"]
+headers = ["Date", "ID", "Comment"]
+wb = Workbook()
+
+#Creates a sheet title for each flair and gives it headers.
+for title in titles:
+    wb.create_sheet(title)
+    page = wb[title]
+    page.append(headers)
+
+#Remove default sheet and save file.
+del wb['Sheet']
+wb.save(filename=workbook_name)
