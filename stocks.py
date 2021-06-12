8af623b2c7f99e9e811a16e206c3adc00308b933
OLGJ <61093694+OLGJ@users.noreply.github.com>
2021-06-12 19:22:39 +0200

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -4,8 +4,11 @@
 import csv
 import os
 
-"""Filters all stocks with a pricing above or equal to 5$ from csv file retrieved 2021-03-29"""
+
 def stocks():
+	"""
+	Filters all stocks with a pricing above or equal to 5$ from csv file retrieved 2021-03-29
+	"""
     with open(r'C:\Users\Olof\Skola\Kurser\729G40\nasdaq_screener_1617017873723.csv', 'r') as csv_stocks:
 
         #Symbol,Name,Last Sale,Net Change,% Change,Market Cap,Country,IPO Year,Volume,Sector,Industry
